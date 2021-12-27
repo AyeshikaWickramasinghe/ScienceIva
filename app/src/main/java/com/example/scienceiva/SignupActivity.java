11b017e0d258315909240afb52f938801ed05577
AyeshikaWickramasinghe <35-se-0035@kdu.ac.lk>
2021-12-27 15:33:33 +0530

    First commit

---
+++ app/src/main/java/com/example/scienceiva/SignupActivity.java
@@ -0,0 +1,113 @@
+package com.example.scienceiva;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.scienceiva.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SignupActivity extends AppCompatActivity {
+
+    ActivitySignupBinding binding;
+    FirebaseAuth auth;
+    FirebaseFirestore database;
+    ProgressDialog dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("We're creating new account...");
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+            }
+        });
+
+        binding.createNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, pass, name;
+
+                email = binding.emailBox.getText().toString();
+                pass = binding.passwordBox.getText().toString();
+                name = binding.nameBox.getText().toString();
+                //referCode = binding.referBox.getText().toString();
+
+                final User user = new User(name, email, pass);
+
+                dialog.show();
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            String uid = task.getResult().getUser().getUid();
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()) {
+                                        //If task complete
+                                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        // Display Toast message "Registration successful"
+                                        Toast.makeText(SignupActivity.this, "User has been registered successfully!", Toast.LENGTH_LONG)
+                                                .show();
+                                    } else {
+                                        Toast.makeText(SignupActivity.this, "Registration Unsuccessful. Try Again!", Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                }
+                            });
+
+                            database
+                                    .collection("users")
+                                    .document(uid)
+                                    .set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        dialog.dismiss();
+//                                        startActivity(new Intent(SignupActivity.this, EvaluationActivity.class));
+//                                        finish();
+                                    } else {
+                                        Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            dialog.dismiss();
+                            Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+    }
+}
