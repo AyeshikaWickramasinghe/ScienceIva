dd7d83359f6a953dc78d0e06665aa6e536ea28dd
AyeshikaWickramasinghe <35-se-0035@kdu.ac.lk>
2022-01-20 22:00:20 +0530

    AR SceneView

--- app/src/main/java/com/example/scienceiva/LandingActivity.java
+++ app/src/main/java/com/example/scienceiva/LandingActivity.java
@@ -21,7 +21,7 @@ public class LandingActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), WaterCycleActivity.class);
                 startActivity(intent);
             }
         });
